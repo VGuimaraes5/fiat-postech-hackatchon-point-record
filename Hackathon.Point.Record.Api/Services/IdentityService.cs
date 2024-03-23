176cb47ecebdf7571cb83275744a046821808f36
Vinicius Guimarães <vg.guimaraes@hotmail.com>
2024-03-22 23:56:47 -0300

    fix

--- Hackathon.Point.Record.Api/Services/IdentityService.cs
+++ Hackathon.Point.Record.Api/Services/IdentityService.cs
@@ -1,19 +1,23 @@
 using System.Security.Claims;
 using Hackathon.Point.Record.Api.Domain.Interfaces.Services;
+using System.Text.Json;
 
 namespace Hackathon.Point.Record.Api.Services;
 
 public class IdentityService : IIdentityService
 {
     private readonly IHttpContextAccessor _context;
+    private readonly ILogger<IdentityService> _logger;
 
-    public IdentityService(IHttpContextAccessor context)
+    public IdentityService(IHttpContextAccessor context, ILogger<IdentityService> logger)
     {
+        _logger = logger;
         _context = context;
     }
 
     public string GetUserIdentification()
     {
+        _logger.LogWarning(JsonSerializer.Serialize(_context.HttpContext?.User));
         var identification = _context.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
         if (string.IsNullOrEmpty(identification)) throw new InvalidDataException("User identification not found!");
